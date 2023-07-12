0ef8adafe3a280f6c32a7160a1190c29e252d356
David Bruant <davidbruant@protonmail.com>
2023-07-12 09:46:23 +0200

    Retour de git Revert "Revert "Utilisation directe de Git (#36)""
    This reverts commit 325d9c0e8ebc7be10b699beba6bddf181cedc4de.

--- assets/scripts/routes/atelier-articles.js
+++ assets/scripts/routes/atelier-articles.js
@@ -15,6 +15,7 @@ import databaseAPI from "../databaseAPI";
 import { svelteTarget } from "../config";
 import { replaceComponent } from "../routeComponentLifeCycle";
 import ArticleContenu from "../components/screens/ArticleContenu.svelte";
+import { setCurrentRepositoryFromQuerystring } from '../actions';
 
 
 const makeMapStateToProps = (fileName) => (state) => {
@@ -24,8 +25,7 @@ const makeMapStateToProps = (fileName) => (state) => {
     file = Promise.resolve(store.state.login).then((login) => {
       return databaseAPI
         .getFile(login, store.state.currentRepository.name, fileName)
-        .then(({ content, sha }) => {
-          const contenu = Buffer.from(content, "base64").toString();
+        .then((contenu) => {
           const {
             attributes: data,
             body: markdownContent,
@@ -37,7 +37,6 @@ const makeMapStateToProps = (fileName) => (state) => {
             previousContent: markdownContent,
             title: data?.title,
             previousTitle: data?.title,
-            sha: sha,
           };
         })
         .catch((msg) => handleErrors(msg));
@@ -47,7 +46,7 @@ const makeMapStateToProps = (fileName) => (state) => {
       imageDirUrl: "",
       contenus: state.articles,
       buildStatus: state.buildStatus,
-      showArticles: state.blogIndexSha !== undefined || state.articles?.length > 0,
+      showArticles: state.pages.find(p => p.path === 'blog.md') !== undefined || state.articles?.length > 0,
       currentRepository: state.currentRepository,
     };
   } else {
@@ -58,12 +57,11 @@ const makeMapStateToProps = (fileName) => (state) => {
         previousContent: undefined,
         title: "",
         previousTitle: undefined,
-        sha: "",
       }),
       imageDirUrl: "",
       contenus: state.articles,
       buildStatus: state.buildStatus,
-      showArticles: state.blogIndexSha !== undefined || state.articles?.length > 0,
+      showArticles: state.pages.find(p => p.path === 'blog.md') !== undefined || state.articles?.length > 0,
       currentRepository: state.currentRepository,
     };
   }
@@ -97,7 +95,7 @@ export default ({ querystring }) => {
       imageDirUrl: `https://github.com/${login}/${state.currentRepository.name}/tree/main/images`,
     });
   });
-  articleContenu.$on("delete", ({ detail: { sha } }) => {
+  articleContenu.$on("delete", () => {
     Promise.resolve(state.login).then((login) => {
       store.mutations.setArticles(
         state.articles.filter((article) => {
@@ -105,19 +103,19 @@ export default ({ querystring }) => {
         })
       );
       databaseAPI
-        .deleteFile(login, state.currentRepository.name, fileName, sha)
+        .deleteFile(login, state.currentRepository.name, fileName)
         .then(() => {
           state.buildStatus.setBuildingAndCheckStatusLater();
-          page(`/atelier-list-pages?repoName=${currentRepository.name}&account=${currentRepository.owner}`);
         })
         .catch((msg) => handleErrors(msg));
     });
+    page(`/atelier-list-pages?repoName=${currentRepository.name}&account=${currentRepository.owner}`);
   });
 
   articleContenu.$on(
     "save",
     ({
-      detail: { fileName, content, previousContent, title, previousTitle, sha },
+      detail: { fileName, content, previousContent, title, previousTitle },
     }) => {
       const hasContentChanged = content !== previousContent;
       const hasTitleChanged = title !== previousTitle;
@@ -136,13 +134,10 @@ export default ({ querystring }) => {
 
       const newFileName = makeArticleFileName(title, date);
 
-      const body = {
-        message: `création de l'article ${title || "index.md"}`,
-        content: Buffer.from(
-          `${title ? makeFrontMatterYAMLJsaisPasQuoiLa(title) + "\n" : ""
-          }${content}`
-        ).toString("base64"),
-      };
+      const message = `création de l'article ${title || "index.md"}`
+      const finalContent =
+        `${title ? makeFrontMatterYAMLJsaisPasQuoiLa(title) + "\n" : ""
+        }${content}`
 
       let newArticles =
         state.articles?.filter((article) => {
@@ -155,37 +150,21 @@ export default ({ querystring }) => {
 
       // If title changed
       if (fileName && fileName !== newFileName) {
-        Promise.resolve(state.login).then((login) => {
-          databaseAPI
-            .updateFile(login, state.currentRepository.name, fileName, newFileName, body, sha)
-            .then(() => {
-              if (body.sha) {
-                console.log("article mise à jour");
-              } else {
-                console.log("nouvel article créé");
-              }
-              state.buildStatus.setBuildingAndCheckStatusLater();
-              page(`/atelier-list-pages?repoName=${currentRepository.name}&account=${currentRepository.owner}`);
-            })
-            .catch((msg) => handleErrors(msg));
-        });
-      } else {
-        Promise.resolve(state.login).then((login) => {
-          body.sha = sha;
-          databaseAPI
-            .createFile(login, state.currentRepository.name, newFileName, body)
-            .then(() => {
-              if (body.sha) {
-                console.log("article mise à jour");
-              } else {
-                console.log("nouvel article créé");
-              }
-              state.buildStatus.setBuildingAndCheckStatusLater();
-              page(`/atelier-list-pages?repoName=${currentRepository.name}&account=${currentRepository.owner}`);
-            })
-            .catch((msg) => handleErrors(msg));
-        });
+        fileName = {
+          old: fileName,
+          new: newFileName
+        }
       }
+
+      Promise.resolve(state.login).then((login) => {
+        databaseAPI
+          .writeFile(login, state.currentRepository.name, fileName, finalContent, message)
+          .then(() => {
+            state.buildStatus.setBuildingAndCheckStatusLater();
+            page(`/atelier-list-pages?repoName=${currentRepository.name}&account=${currentRepository.owner}`);
+          })
+          .catch((msg) => handleErrors(msg));
+      });
     }
   );
 };
